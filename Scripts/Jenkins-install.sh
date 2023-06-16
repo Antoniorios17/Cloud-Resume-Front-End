c931dcfb241268426301d028e465e3473324eeb6
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-06-16 18:58:45 -0400

    jenkins script update

--- Scripts/Jenkins-install.sh
+++ Scripts/Jenkins-install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#This script will automatically install jenkins
+#it will not do any configuration
+
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
